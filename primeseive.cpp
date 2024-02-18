b38ed46888b4b979873afe66cf7dbb80a7a2246c
mohd-asad <110733847+mohd-asad@users.noreply.github.com>
2024-02-18 23:37:30 +0530

    Update primeseive.cpp

--- primeseive.cpp
+++ primeseive.cpp
@@ -1,15 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void SieveOfEratosthenes(int n)
-{
-    
+void SieveOfEratosthenes(int n){
     vector<int> prime(n,1);
- 
     for (int p = 2; p * p <= n; p++) {
-        
         if (prime[p] == true) {
-           
             for (int i = p * p; i <= n; i += p)
                 prime[i] = false;
         }
